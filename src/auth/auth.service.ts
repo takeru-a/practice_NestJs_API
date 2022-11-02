f0646ae8bd556d6abfcbc9e823e8bf126bae4b16
takeru-a <pypy20191022@gmail.com>
2022-11-02 17:52:07 +0900

    login & signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { Injectable , ForbiddenException} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService){}
-    signup(){
-        return {'msg': 'I am signed up'};
-
+    async signup(dto: AuthDto){
+        //パスワードのハッシュ化
+        const hash = await argon.hash(dto.password);
+        // DBにユーザ情報を保存
+        try{
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+            
+        });
+        delete user.hash;
+        return user;
+    }catch(error){
+        if(error instanceof Prisma.PrismaClientKnownRequestError){
+            if (error.code === 'P2002'){
+            throw new ForbiddenException(
+                'Credentials taken',
+            );
+            }
+        }
+        throw error;
     }
-    signin(){
-        return{'msg': 'I am signed in'};
+    }
+    async signin(dto: AuthDto){
+        const user = await this.prisma.user.findUnique({
+            where:{
+                email: dto.email,
+            },
+        });
+        // ユーザがいない場合のエラー処理
+        if (!user) throw new ForbiddenException(
+            'Credentials incorrect'
+        );
+
+        //パスワードの検証
+        const pwMatches = await argon.verify(
+            user.hash,
+            dto.password,
+        );
+        // パスワードが不正な場合のエラー処理
+        if (!pwMatches) throw new ForbiddenException(
+            'Credentials incorrect'
+        );
+
+        delete user.hash;
+        return user;
     }
 }
